4f33ced2861598684047e179ede57cf8bdea7cee
jony-sarowar <jonysarowar@gmail.com>
2025-06-20 00:03:21 +0600

    Initial commit

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 // Static files and views
 app.use('/uploads', express.static('uploads'));
